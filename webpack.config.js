5444868065df38171a6193e2fd8903bcc350149a
Michael W Macbook <mwegter95@gmail.com>
2022-02-16 00:52:24 -0600

    added webpack and set it up

---
+++ webpack.config.js
@@ -0,0 +1,17 @@
+const path = require("path");
+const webpack = require("webpack");
+// require("bootstrap");
+
+module.exports = {
+    entry: './assets/js/script.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'main.bundle.js'
+    },
+    mode: 'development',
+    plugins: [new webpack.ProvidePlugin({
+        $: "jquery",
+        jQuery: "jquery"
+    }),
+],
+}
